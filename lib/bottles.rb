230d183f73468e8529b1d27ff85c3fac6b555cd2
Josh Rowley <joshua.f.rowley@gmail.com>
2018-05-22 03:25:58 -0400

    refactor container method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,6 @@
 class Bottles
   def verse(number)
     case number
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" +
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
@@ -12,7 +9,7 @@ class Bottles
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" + 
-      "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+      "Take one down and pass it around, #{number - 1} #{container(number -1)} of beer on the wall.\n"
     end
   end
 
@@ -22,5 +19,15 @@ class Bottles
 
   def song
     verses(99, 0)
+  end
+
+  private
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
   end
 end
